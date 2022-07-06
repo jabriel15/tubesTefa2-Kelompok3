cd112940c919ea3b5b5ad5de04fce3a9a2007170
___jb <67944576+jabriel15@users.noreply.github.com>
2022-07-06 17:36:27 +0700

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# tubesTefa2-Kelompok3
-Tugas akhir TeFa chapter 2 kelompok 3
+# TuBes-Chapter2-Kelompok3
+
+## Latar Belakang
+Cafe Internet I-Coffee ingin meningkatkan pelayanan untuk pelanggan reguler
+dan pelanggan setianya, jadi ketika pelanggan tersebut ingin memesan, dia
+bisa memilih bahwa dia pelanggan reguler atau VIP, jika dia pelanggan VIP
+dia akan memasukkan nomor pelanggan terlebih dahulu sedangkan
+pelanggan reguler bisa langsung memilih menunya, untuk harga pelanggan
+VIP setiap makanan akan dikurangi sebesar 15% dan untuk minuman
+dikurangi sebesar 10%. Buat aplikasi pemesanan ini dengan minimal ada 2
+jenis makanan dan 2 jenis minuman yang ada di menu dengan harga yang
+berbeda beda, serta bisa melakukan perhitungan dari menu yang dipesan.
+Pesanan dan Harga akan muncul di Modal / Popup ketika submit.
+
+## Website Cafe Internet
+Website ini berfungsi untuk mempermudah pelanggan dalam memesan makanan atau minuman, dan mempermudah penjual dalam melayani pelanggan regular dan pelanggan setianya atau pelanggan VIP
+
+## Features
+
+- Memisahkan antara pelanggan Regular dan pelanggan VIP
+- Memesan makanan atau minuman
+- Memberikan potongan harga khusus pelanggan VIP
+
+Fitur fitur di atas merupakan inti dari website I-Coffee ini.
+
+
+## Tech
+
+Website ini dibuat dengan menggunakan:
+
+| Technology | Description                               |
+|------------|-------------------------------------------|
+| HTML5      | Hyper Text Markup Language                |
+| CSS3       | Cascading Style Sheets                    |
+| JavaScript | High Level, Dynamic, Interpreted Language |
+
+
+## Instalation
+Clone Repository ini dengan cara:<br>
+
+```
+git clone https://github.com/jabriel15/TuBes-Chapter2-Kelompok3.git
+```
+
+Buka file index.html di dalam folder Web I-Coffee
+
+
+## Screnshoots
+#### Home
+<img src="img/home.png">
+<br><br>
+
+#### VIP
+<img src="img/vip.png">
+<br><br>
+
+#### Menu VIP
+<img src="img/menuvip.png">
+<br><br>
+
+#### Menu
+<img src="img/menu.png">
+
+## Credit
+>Adhi Puspo Hadikusumo <br>
+>Bimo Rahman Hafidz <br>
+>Jabriel Bamasena J.R <br>
+>Muhammad Omar Nadiv
---
+++ js/regular.js
@@ -0,0 +1,147 @@
+
+// Fungsi cart
+let carts = document.querySelectorAll('.add-cart');
+
+let products = [
+    {
+        name: 'Mie Goreng',
+        tag: 'miegoreng',
+        price: 20000,
+        inCart: 0
+    },
+    {
+        name: 'Mie Kuah',
+        tag: 'miekuah',
+        price: 22000,
+        inCart: 0
+    },
+    {
+        name: 'Iced Coffeemilk',
+        tag: 'icedcoffee',
+        price: 15000,
+        inCart: 0
+    },
+    {
+        name: 'Americano',
+        tag: 'americano',
+        price: 19000,
+        inCart: 0
+    }
+]
+
+for (let i=0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i]);
+    })
+}
+
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+
+function cartNumbers(product) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    productNumbers = parseInt(productNumbers);
+
+    if(productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+
+    setItems(product);
+}
+
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if(cartItems != null ) {
+        if(cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+// Total harga per item
+function totalCost(product) {
+    // console.log("Harganya adalah", product.price);
+    let cartCost = localStorage.getItem('totalCost');
+
+    console.log("My CartCost Is", cartCost);
+    console.log(typeof cartCost);
+
+    if(cartCost != null){
+        cartCost = parseInt(cartCost)
+        localStorage.setItem('totalCost', cartCost + product.price);
+    } else {
+        localStorage.setItem('totalCost', product.price);
+    }
+}
+
+// Display di popup 
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let containerProduct = document.querySelector(".products");
+    let cartCost = localStorage.getItem('totalCost');
+
+    console.log(cartItems);
+    if(cartItems && containerProduct ) {
+        containerProduct.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            containerProduct.innerHTML += `
+            <div class="product">${item.name}</div>
+            <div class="price">Rp.${item.price}</div>
+            <div class="quantity">${item.inCart}</div>
+            <div class="total">Rp.${item.inCart * item.price}</div>
+            `
+        });
+
+        containerProduct.innerHTML += `
+            <div class="cartTotalContainer">
+                <p class="cartTotalTitle">
+                    Total Price:
+                </p>
+                <p class="cartTotal">
+                    Rp.${cartCost}
+                </p>
+        `
+
+    }
+}
+
+function hapus(){
+    localStorage.clear();
+    location.reload();
+}
+
+function bayar() {
+    alert("Payment Success")
+    localStorage.clear();
+    location.reload();
+}
+
+
+onLoadCartNumbers();
+displayCart();
